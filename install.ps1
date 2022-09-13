4aceb6ac3b54eea769a0849db8994ccbc037ee9a
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2022-09-13 21:27:45 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip -O $env:TEMP\apache-maven.zip
+#Invoke-WebRequest https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip -O $env:TEMP\apache-maven.zip
 
 Expand-Archive -Path $env:TEMP\apache-maven.zip -DestinationPath $env:TEMP\apache-maven -Force
 
