5b2ec4aea11f5726604f91617e14ff053f46a775
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2022-09-13 21:49:04 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,15 +1,12 @@
-#Invoke-WebRequest https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip -O $env:TEMP\apache-maven.zip
+Invoke-WebRequest https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip -O $env:TEMP\apache-maven.zip
 
 Expand-Archive -Path $env:TEMP\apache-maven.zip -DestinationPath $env:TEMP\apache-maven -Force
 
 Copy-Item -Path $env:TEMP\apache-maven\apache-maven-*\* -Destination c:\apache-maven -Force
 
-if ( (Get-ChildItem -Path Env:Path).Value -split ';'  -contains  'c:\apache-maven\bin' -eq $false)
-{
+if ( (Get-ChildItem -Path Env:Path).Value -split ';'  -contains  'c:\apache-maven\bin' -eq $false) {
 Write-Host "Adding 'c:\apache-maven\bin' to the path"
-setx Path ('c:\apache-maven\bin;' + [Environment]::GetEnvironmentVariable('Path'))
-}
-else 
-{
+setx Path ('c:\apache-maven\bin;' + [Environment]::GetEnvironmentVariable('Path', 'User'))
+} else {
 Write-Host "'c:\apache-maven\bin' is already in the path"
 }
