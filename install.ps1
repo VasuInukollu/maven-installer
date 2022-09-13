11906f17fab56344a445d50d4cbe6d5b9edb2d5e
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2022-09-13 21:13:49 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,11 @@
+Invoke-WebRequest https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip -O $env:TEMP\apache-maven.zip
+
+Expand-Archive -Path $env:TEMP\apache-maven.zip -DestinationPath $env:TEMP\apache-maven
+
+Move-Item -Path $env:TEMP\apache-maven\apache-maven-*\* -Destination c:\apache-maven
+
+if ( (Get-ChildItem -Path Env:Path).Value -split ';'  -contains  'c:\apache-maven\bin' -eq $false)
+{
+setx Path ('c:\apache-maven\bin;' + [Environment]::GetEnvironmentVariable('Path'))
+}
+
